88d85e44f1a224f4c419dbc3c26205ad1e0b65f7
Parsifal-2045 <ferragina.luca@gmail.com>
2023-05-11 22:26:16 +0200

    Add first version of 1/R fit

--- analysis/analysis.cpp
+++ analysis/analysis.cpp
@@ -322,61 +322,125 @@ void advanced_fit()
     gStyle->SetOptTitle(1);
     gStyle->SetOptStat(1110);
     gStyle->SetOptFit(111);
+    gStyle->SetLineScalePS(1);
 
-    // char benchmark;
-    // std::cout << "Do you want to benchmark the analysis code? (y/n)" << '\n';
-    // std::cin >> benchmark;
-    if (0) // benchmark == 'y')
+    // retrieve histograms
+    TFile *file = new TFile("filtered_data.root", "READ");
+    TH1D *filtered_histograms[4];
+
+    TH1D *p1_and_p2_iron = (TH1D *)file->Get("P2_filtered_iron");
+    filtered_histograms[0] = p1_and_p2_iron;
+    p1_and_p2_iron->SetTitle("P1 and P2 coincidences iron");
+    TH1D *p3_filtered_iron = (TH1D *)file->Get("P3_filtered_iron");
+    filtered_histograms[1] = p3_filtered_iron;
+
+    TH1D *p1_and_p2_cement = (TH1D *)file->Get("P2_filtered_cement");
+    filtered_histograms[2] = p1_and_p2_cement;
+    p1_and_p2_cement->SetTitle("P1 and P2 coincidences cement");
+    TH1D *p3_filtered_cement = (TH1D *)file->Get("P3_filtered_cement");
+    filtered_histograms[3] = p3_filtered_cement;
+    for (int i = 0; i != 4; i++)
     {
-        gBenchmark->Start("Analysis Benchmark");
-    }
-
-    TFile *file = new TFile("all_data.root", "READ");
-    TH1D *histograms[6];
-
-    TH1D *p1_iron = (TH1D *)file->Get("P1_iron");
-    histograms[0] = p1_iron;
-    TH1D *p2_iron = (TH1D *)file->Get("P2_iron");
-    histograms[1] = p2_iron;
-    TH1D *p3_iron = (TH1D *)file->Get("P3_iron");
-    histograms[2] = p3_iron;
-
-    TH1D *p1_cement = (TH1D *)file->Get("P1_cement");
-    histograms[3] = p1_cement;
-    TH1D *p2_cement = (TH1D *)file->Get("P2_cement");
-    histograms[4] = p2_cement;
-    TH1D *p3_cement = (TH1D *)file->Get("P3_cement");
-    histograms[5] = p3_cement;
-
-    for (int i = 0; i != 6; i++)
-    {
-        histograms[i]->GetXaxis()->SetTitle("Stop time (#mus)");
-        histograms[i]->GetYaxis()->SetTitle("Counts");
+        filtered_histograms[i]->GetXaxis()->SetTitle("Stop time (#mus)");
+        filtered_histograms[i]->GetXaxis()->SetTitleSize(0.045);
+        filtered_histograms[i]->GetYaxis()->SetTitle("Counts");
+        filtered_histograms[i]->GetYaxis()->SetTitleSize(0.045);
     }
 
     // 1/R exponential -> free parameters: N(u+, 0), lifetime, charge rate R, capture time, background
-    auto adv_exp = new TF1("adv_exp", "[0]*TMath::Exp(-x*[1])*(1 + 1/[2]*TMath::Exp(-x*[3])) + [4]");
-    adv_exp->SetParameters(200, 0.5, 1.27, 5, 6);
+    auto adv_exp = new TF1("adv_exp", "[0]*TMath::Exp(-x/[1])*(1 + 1/[2]*TMath::Exp(-x/[3])) + [4]");
+    adv_exp->SetParameters(200, 2, 1, 0.2, 6);
+    adv_exp->SetParNames("N(#mu^{+}, 0)", "#tau_{0}", "R", "#tau_{c}", "b");
 
-    // P3 cement fit and parameters
-    p3_cement->Fit(adv_exp, "E,M,Q", "", 0.1, 12);
-    auto one_over_tau = adv_exp->GetParameter(1);
-    auto err_one_over_tau = adv_exp->GetParError(1);
-    auto tau = 1 / one_over_tau;
-    auto err_tau = tau * (err_one_over_tau / one_over_tau);
-    std::cout << red << "P3 Cement fit parameters:" << reset << '\n';
-    std::cout << cyan << tau << " +/- " << err_tau << '\n';
+    ////////////////////////////////////////////////
+    /////////////////// IRON ///////////////////////
+    ////////////////////////////////////////////////
+
+    // P1 and P2 coincidences iron fit and parameters
+    p1_and_p2_iron->Fit(adv_exp, "E,M,Q", "", 0.07, 8);
+    std::cout << red << "P1 and P2 coincidences Iron fit parameters with 1/R fit:" << reset << '\n';
+    std::cout << cyan << adv_exp->GetParameter(1) << " +/- " << adv_exp->GetParError(1) << '\n';
     std::cout << "chi2 / NDF: " << adv_exp->GetChisquare() << "/" << adv_exp->GetNDF() << reset << std::endl;
 
-    auto adv_exp_canvas = new TCanvas("advanced_fit_display", "Fit iron and cement exponential");
-    adv_exp_canvas->Divide(3, 2);
-    for (int i = 0; i != 6; i++)
+    // P3 filtered iron fit and parameters
+    p3_filtered_iron->Fit(adv_exp, "E,M,Q", "", 0.07, 8);
+    std::cout << red << "Filtered P3 Iron fit parameters with 1/R fit:" << reset << '\n';
+    std::cout << cyan << adv_exp->GetParameter(1) << " +/- " << adv_exp->GetParError(1) << '\n';
+    std::cout << "chi2 / NDF: " << adv_exp->GetChisquare() << " / " << adv_exp->GetNDF() << reset << std::endl;
+
+    TPaveStats *stats_iron[2];
+    TPaveStats *st1_iron = (TPaveStats *)p1_and_p2_iron->FindObject("stats");
+    stats_iron[0] = st1_iron;
+    st1_iron->SetName("p1_and_p2_iron_stats");
+    TPaveStats *st3_iron = (TPaveStats *)p3_filtered_iron->FindObject("stats");
+    stats_iron[1] = st3_iron;
+    st3_iron->SetName("p3_filtered_iron_stats");
+    for (int i = 0; i != 2; i++)
     {
-        adv_exp_canvas->cd(i + 1);
-        histograms[i]->Draw("E,H,SAME");
+        stats_iron[i]->SetX1NDC(0.582544);
+        stats_iron[i]->SetY1NDC(0.545131);
+        stats_iron[i]->SetX2NDC(0.901034);
+        stats_iron[i]->SetY2NDC(0.901298);
     }
-    if (0) // benchmark == 'y')
+
+    ////////////////////////////////////////////////
+    ////////////////// CEMENT //////////////////////
+    ////////////////////////////////////////////////
+
+    // P1 and P2 coincidences cement fit and parameters
+    p1_and_p2_cement->Fit(adv_exp, "E,M,Q", "", 0.1, 12);
+    std::cout << red << "P1 and P2 coincidences Cement fit parameters with 1/R fit:" << reset << '\n';
+    std::cout << cyan << adv_exp->GetParameter(1) << " +/- " << adv_exp->GetParError(1) << '\n';
+    std::cout << "chi2 / NDF: " << adv_exp->GetChisquare() << "/" << adv_exp->GetNDF() << reset << std::endl;
+
+    // P3 filtered cement fit and parameters
+    p3_filtered_cement->Fit(adv_exp, "E,M,Q", "", 0.1, 12);
+    std::cout << red << "Filtered P3 Cement fit parameters with 1/R fit:" << reset << '\n';
+    std::cout << cyan << adv_exp->GetParameter(1) << " +/- " << adv_exp->GetParError(1) << '\n';
+    std::cout << "chi2 / NDF: " << adv_exp->GetChisquare() << "/" << adv_exp->GetNDF() << reset << std::endl;
+
+    TPaveStats *stats_cement[2];
+    TPaveStats *st1_cement = (TPaveStats *)p1_and_p2_cement->FindObject("stats");
+    stats_cement[0] = st1_cement;
+    st1_cement->SetName("p1_and_p2_cement_stats");
+    TPaveStats *st3_cement = (TPaveStats *)p3_filtered_cement->FindObject("stats");
+    stats_cement[1] = st3_cement;
+    st3_cement->SetName("p3_filtered_cement_stats");
+    for (int i = 0; i != 2; i++)
     {
-        gBenchmark->Show("Analysis Benchmark");
+        stats_cement[i]->SetX1NDC(0.582544);
+        stats_cement[i]->SetY1NDC(0.545131);
+        stats_cement[i]->SetX2NDC(0.901034);
+        stats_cement[i]->SetY2NDC(0.901298);
     }
+
+    // Visualize all histograms
+    auto filtered_canvas = new TCanvas("filtered_canvas", "Filtered iron and cement fit with with 1/R exponential");
+    filtered_canvas->Divide(2, 2);
+    for (int i = 0; i != 4; i++)
+    {
+        filtered_canvas->cd(i + 1);
+        filtered_histograms[i]->Draw("E,H");
+    }
+
+    auto iron_filtered_canvas = new TCanvas("iron_filtered_canvas", "Iron filtered fit - 1/R exponential fit");
+    iron_filtered_canvas->Divide(1, 2);
+    for (int i = 0; i != 2; i++)
+    {
+        iron_filtered_canvas->cd(i + 1);
+        filtered_histograms[i]->Draw("E,H");
+    }
+
+    auto cement_filtered_canvas = new TCanvas("cement_filtered_canvas", "Cement filtered - 1/R exponential fit");
+    cement_filtered_canvas->Divide(1, 2);
+    int j = 1;
+    for (int i = 2; i != 4; i++)
+    {
+        cement_filtered_canvas->cd(j);
+        filtered_histograms[i]->Draw("E,H");
+        j++;
+    }
+
+    iron_filtered_canvas->Print("1_over_r_iron_fit.pdf");
+    cement_filtered_canvas->Print("1_over_r_cement_fit.pdf");
 }
